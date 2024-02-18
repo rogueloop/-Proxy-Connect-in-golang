bfa0a0418677170555c32cb92f7d4f52d3b44918
Sreedeep CV <sreedeep200@gmail.com>
2024-02-18 10:45:58 +0530

    init repo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module newServer
+
+go 1.21.1
+
+require golang.org/x/net v0.21.0
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func ProxyAwareHttpClient() (*http.Client, error) {
+	proxyURL, err := url.Parse("http://proxyserver.hexmos.com:8080/")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Invalid proxy url:", err)
+		return nil, err
+	}
+
+	proxyURL.User = url.UserPassword("proxyServer", "proxy22523146server")
+
+	// Setup an HTTP client with a proxy transport
+	proxyTransport := &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+	httpClient := &http.Client{Transport: proxyTransport}
+	return httpClient, nil
+}
+
+func main() {
+	client, err := ProxyAwareHttpClient()
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("GET", "https://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	contents, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(contents))
+}
